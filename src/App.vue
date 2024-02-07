898d273e81d8e282ad1761c527c578ad82ea71f1
scarlett-troncoso <scar.labussieri@gmail.com>
2024-02-07 15:01:03 +0100

    initial tags

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style ></style>
