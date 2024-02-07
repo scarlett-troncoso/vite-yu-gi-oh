b9ad433b9813ed0b2b2b7dd65450c25568b079b2
scarlett-troncoso <scar.labussieri@gmail.com>
2024-02-07 16:26:38 +0100

    add request ajax api yu gi oh

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,35 @@ export default {
   name: 'App',
   data() {
     return {
-
+      url_api: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+      characters: [],
     }
+  },
+  mounted() {
+    console.log((this.url_api));
+
+    axios
+      .get(this.url_api)
+      .then((response) => {
+        console.log(response.data);
+        console.log(response.data.data);
+        //name e archetype e imagine ygoprodeck_url
+        this.characters = response.data.data
+
+      })
   }
 }
 </script>
 
 <template>
-  <div>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-5">
+        <div class="card" v-for="character in characters">
+          {{ character.name }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
