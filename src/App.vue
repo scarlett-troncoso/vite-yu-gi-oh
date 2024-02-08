6779c75929d677455a23e4b71161fce3d2153ffa
scarlett-troncoso <scar.labussieri@gmail.com>
2024-02-08 11:48:48 +0100

    modify link api with num and offset

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ export default {
   name: 'App',
   data() {
     return {
-      url_api: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+      url_api: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=39&offset=0&archetype=Alien',
       characters: [],
     }
   },
@@ -17,20 +17,23 @@ export default {
       .then((response) => {
         console.log(response.data);
         console.log(response.data.data);
-        //name e archetype e imagine ygoprodeck_url
         this.characters = response.data.data
 
       })
   }
 }
+//non ho capito bene come diminuire il numero di risultati con num e offset
 </script>
-
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-5">
+      <div class="col-4">
         <div class="card" v-for="character in characters">
-          {{ character.name }}
+          <div v-for="items in character.card_images" class="cont-img">
+            <img :src='items.image_url' alt="#">
+          </div>
+          <h3>{{ character.name }}</h3>
+          <span>{{ character.archetype }}</span>
         </div>
       </div>
     </div>
