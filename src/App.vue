89cf3f64d942424c36a1f246051040b5eed0b33f
scarlett-troncoso <scar.labussieri@gmail.com>
2024-02-08 14:02:23 +0100

    add styles

--- src/App.vue
+++ src/App.vue
@@ -22,22 +22,24 @@ export default {
       })
   }
 }
-//non ho capito bene come diminuire il numero di risultati con num e offset
 </script>
+
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-4">
-        <div class="card" v-for="character in characters">
-          <div v-for="items in character.card_images" class="cont-img">
-            <img :src='items.image_url' alt="#">
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <div class="card" v-for="character in characters">
+            <div v-for="items in character.card_images" class="cont-img">
+              <img :src='items.image_url' alt="#">
+            </div>
+            <h3>{{ character.name }}</h3>
+            <span>{{ character.archetype }}</span>
           </div>
-          <h3>{{ character.name }}</h3>
-          <span>{{ character.archetype }}</span>
         </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <style ></style>
