d07f32b6b4891e3a43db40d5d134173a1913640d
scarlett-troncoso <scar.labussieri@gmail.com>
2024-02-09 21:47:40 +0100

    change urla api

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ export default {
   name: 'App',
   data() {
     return {
-      url_api: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=39&offset=0&archetype=Alien',
+      url_api: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=40&offset=0', //https://db.ygoprodeck.com/api/v7/cardinfo.php?num=39&offset=0&archetype=Alien',
       characters: [],
     }
   },
@@ -17,6 +17,7 @@ export default {
       .then((response) => {
         console.log(response.data);
         console.log(response.data.data);
+        console.log(response.data.data[0].name);
         this.characters = response.data.data
 
       })
@@ -27,11 +28,25 @@ export default {
 <template>
   <main>
     <div class="container">
+      <div class="filters">
+        <!--<input type="" placeholder="Type a name to search">-->
+        <select name="archetipo" id="archetipo">
+          <option value="all" selected>All</option>
+          <option value="alien">alien</option>
+          <option value="Infernoble Arms">Infernoble Arms</option>
+          <option value="Noble Knight
+">Noble Knight</option>
+        </select>
+      </div>
       <div class="row">
         <div class="col-3">
           <div class="card" v-for="character in characters">
+            <!-- COSI HO FATTO PRIMA MA NON ERA GIUSTO
             <div v-for="items in character.card_images" class="cont-img">
               <img :src='items.image_url' alt="#">
+            </div>-->
+            <div class="cont-img">
+              <img :src='character.card_images[0].image_url' alt="#">
             </div>
             <h3>{{ character.name }}</h3>
             <span>{{ character.archetype }}</span>
